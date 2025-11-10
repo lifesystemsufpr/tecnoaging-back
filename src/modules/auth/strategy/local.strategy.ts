9163332d1ce50ab2575b83f90229caaeadce8dfa
Lucca Chella <lucca.hmcps@gmail.com>
2025-11-10 18:40:16 -0300

    feat: refresh token auth via cookie + alteração no login, agora username -> cpf

--- src/modules/auth/strategy/local.strategy.ts
+++ src/modules/auth/strategy/local.strategy.ts
@@ -2,20 +2,23 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import { Payload } from '../interfaces/auth.interface';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
+    super({
+      usernameField: 'cpf',
+    });
   }
 
-  async validate(username: string, password: string): Promise<any> {
-    const user = await this.authService.validateCredentials(username, password);
+  async validate(cpf: string, password: string): Promise<Payload> {
+    const user = await this.authService.validateCredentials(cpf, password);
 
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Credenciais inválidas');
     }
 
-    return user;
+    return user as Payload;
   }
 }
