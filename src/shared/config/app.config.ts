a2a048e5704fe5a15540954f90b1859d0db00d6d
Lucca Chella <lucca.hmcps@gmail.com>
2025-11-19 18:40:53 -0300

    feat: correção de tempo de expiração token

--- src/shared/config/app.config.ts
+++ src/shared/config/app.config.ts
@@ -1,54 +1,10 @@
 import { Logger } from '@nestjs/common';
 import { AppConfig, SecurityConfig } from './config.interface';
 
-// A biblioteca JWT espera o tempo de expiração em segundos quando é um número.
 const FIFTEEN_MINUTES_IN_SECONDS = 15 * 60; // 900
 const SEVEN_DAYS_IN_SECONDS = 7 * 24 * 60 * 60; // 604800
 
-const securityConfig: SecurityConfig = {
-  jwtSecret: process.env.JWT_SECRET || 'defaultSecret',
-
-  jwtExpirationTime: process.env.JWT_EXPIRES_IN
-    ? +process.env.JWT_EXPIRES_IN
-    : FIFTEEN_MINUTES_IN_SECONDS,
-
-  jwtRefreshExpirationTime: process.env.JWT_REFRESH_EXPIRES_IN
-    ? +process.env.JWT_REFRESH_EXPIRES_IN
-    : SEVEN_DAYS_IN_SECONDS,
-};
-
-const appConfig: AppConfig = {
-  nest: {
-    port: process.env.NEST_PORT ? +process.env.NEST_PORT : 3333,
-    environment: process.env.NODE_ENV || 'development',
-    httpsEnabled: process.env.HTTPS_ENABLED === 'true',
-    sslKeyPath: process.env.SSL_KEY_PATH,
-    sslCertPath: process.env.SSL_CERT_PATH,
-  },
-  swagger: {
-    title: process.env.SWAGGER_TITLE || 'tecnoaging-web',
-    description: process.env.SWAGGER_DESCRIPTION || 'API Documentation',
-    version: process.env.SWAGGER_VERSION || '1.0.0',
-    path: process.env.SWAGGER_PATH || 'api-docs',
-    enabled: process.env.SWAGGER_ENABLED
-      ? Boolean(process.env.SWAGGER_ENABLED)
-      : true,
-  },
-  cors: {
-    enabled: process.env.CORS_ENABLED
-      ? Boolean(process.env.CORS_ENABLED)
-      : false,
-    corsOrigins: getCorsOrigins(process.env.CORS_ORIGINS),
-  },
-  security: securityConfig,
-};
-
-export default () => {
-  const logger = new Logger('AppConfig');
-  logger.log(`Loaded application configuration: ${JSON.stringify(appConfig)}`);
-  return appConfig;
-};
-
+// eslint-disable-next-line sonarjs/function-return-type
 function getCorsOrigins(
   corsOrigins: string | undefined,
 ): boolean | string | string[] {
@@ -63,3 +19,48 @@ function getCorsOrigins(
   }
   return corsOrigins.split(',').map((origin) => origin.trim());
 }
+
+export default () => {
+  const securityConfig: SecurityConfig = {
+    jwtSecret: process.env.JWT_SECRET || 'defaultSecret',
+
+    jwtExpirationTime: process.env.JWT_EXPIRES_IN
+      ? +process.env.JWT_EXPIRES_IN
+      : FIFTEEN_MINUTES_IN_SECONDS,
+
+    jwtRefreshExpirationTime: process.env.JWT_REFRESH_EXPIRES_IN
+      ? +process.env.JWT_REFRESH_EXPIRES_IN
+      : SEVEN_DAYS_IN_SECONDS,
+  };
+
+  const appConfig: AppConfig = {
+    nest: {
+      port: process.env.NEST_PORT ? +process.env.NEST_PORT : 3333,
+      environment: process.env.NODE_ENV || 'development',
+      httpsEnabled: process.env.HTTPS_ENABLED === 'true',
+      sslKeyPath: process.env.SSL_KEY_PATH,
+      sslCertPath: process.env.SSL_CERT_PATH,
+    },
+    swagger: {
+      title: process.env.SWAGGER_TITLE || 'tecnoaging-web',
+      description: process.env.SWAGGER_DESCRIPTION || 'API Documentation',
+      version: process.env.SWAGGER_VERSION || '1.0.0',
+      path: process.env.SWAGGER_PATH || 'api-docs',
+      enabled: process.env.SWAGGER_ENABLED
+        ? Boolean(process.env.SWAGGER_ENABLED)
+        : true,
+    },
+    cors: {
+      enabled: process.env.CORS_ENABLED
+        ? Boolean(process.env.CORS_ENABLED)
+        : false,
+      corsOrigins: getCorsOrigins(process.env.CORS_ORIGINS),
+    },
+    security: securityConfig,
+  };
+
+  const logger = new Logger('AppConfig');
+  logger.log(`Loaded application configuration: ${JSON.stringify(appConfig)}`);
+
+  return appConfig;
+};
