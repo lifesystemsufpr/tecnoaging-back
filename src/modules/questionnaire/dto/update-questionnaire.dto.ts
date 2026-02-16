c08cc49c681dcce834845e629c13a3f1cb7a1f95
Lucca Chella <lucca.hmcps@gmail.com>
2026-02-15 22:09:01 -0300

    chore(questionnaires): remocao completa de logica + questionnaires do banco, falta executar migration

--- src/modules/questionnaire/dto/update-questionnaire.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateQuestionnaireDto } from './create-questionnaire.dto';
-
-// O PartialType torna todos os campos do Create opcionais automaticamente
-export class UpdateQuestionnaireDto extends PartialType(
-  CreateQuestionnaireDto,
-) {}
